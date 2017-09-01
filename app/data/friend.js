bf3a8442f50db8fffabe76ee2567025375c75c44
Chinedu Onugu <teddybluez@users.noreply.github.com>
2017-09-01 01:51:14 -0400

    Friend-Finder

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+
+var friendList = [
+  {
+    name: 'teddybluez',
+    profilePic: 'https://avatars3.githubusercontent.com/u/26339865?',
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  }
+]
+
+module.exports = friendList;
